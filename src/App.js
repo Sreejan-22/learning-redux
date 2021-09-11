ee287c985cd717b8915452da6ab62f55cfff9343
Sreejan <sreejan147@gmail.com>
2021-09-12 01:07:48 +0530

    feat: added reducer | updated App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <div>Redux</div>
+      <div>Hello Redux</div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
+// External imports
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+// Local imports
 import App from "./App";
+import rootReducer from "./reducers";
+
+// assets
+import "./index.css";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
